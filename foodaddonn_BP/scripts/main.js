3a65d3b929b674e93f061ae7aceef2fc8736e36d
momijidaaa <m89184412@gmail.com>
2025-12-30 16:06:38 +0900

    Update main.js

--- foodaddonn_BP/scripts/main.js
+++ foodaddonn_BP/scripts/main.js
@@ -1,2 +1,19 @@
 import * as server from "@minecraft/server";
 import * as ui from "@minecraft/server-ui";
+
+
+import { world, ItemStack, Player } from "@minecraft/server";
+
+world.afterEvents.itemUse.subscribe((event) => {
+    const { source: player, itemStack } = event;
+
+    if (itemStack.typeId === "minecraft:compass") {
+        
+        if (player instanceof Player) {
+            player.addExperience(10);
+            
+            player.sendMessage("§a経験値を10獲得しました！");
+        }
+    }
+});
+
