5ae48d7eb05a3a1cdb05f4f9ae952c12fd342ea0
momijidaaa <m89184412@gmail.com>
2025-12-31 16:53:17 +0900

    ｂ

--- foodaddonn_BP/scripts/main.js
+++ foodaddonn_BP/scripts/main.js
@@ -1,7 +1,7 @@
 import * as server from "@minecraft/server";
 
 server.world.beforeEvents.worldInitialize.subscribe(init => {
-   init.itemComponentRegistry.registerCustomComponent("mc:honeybread_component", {
+   init.itemComponentRegistry.registerCustomComponent("mc:honeybread", {
     onConsume(arg) {
       const player = arg.source;
       player.addEffect("speed", 60, { amplifier: 1, showParticles: true });
