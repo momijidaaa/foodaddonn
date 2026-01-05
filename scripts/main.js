d1f041ad0fa60040e39d1beec01848e79f5070d2
momijidaaa <m89184412@gmail.com>
2026-01-05 13:00:10 +0900

    ver itigyounimatometayontinitnunkomankooppaiuiiiiiiiiiokokokok a

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,29 @@
 import * as server from "@minecraft/server";
 
 server.world.beforeEvents.worldInitialize.subscribe(init => {
-   init.itemComponentRegistry.registerCustomComponent("mc:honeybread", {
+
+  init.itemComponentRegistry.registerCustomComponent("mc:honeybread", {
     onConsume(arg) {
       const player = arg.source;
-      player.addEffect("speed", 60, { amplifier: 1, showParticles: true });
+      if (!(player instanceof server.Player)) return;
+
+      player.addEffect("speed", 60, {
+        amplifier: 1,
+        showParticles: true
+      });
     }
-   });
+  });
+
+  init.itemComponentRegistry.registerCustomComponent("mc:cooked_catfish", {
+    onConsume(arg) {
+      const player = arg.source;
+      if (!(player instanceof server.Player)) return;
+
+      player.addEffect("water_breathing", 100, {
+        amplifier: 2,
+        showParticles: true
+      });
+    }
+  });
+
 });
