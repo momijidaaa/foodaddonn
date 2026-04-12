f33a0592ffd9bd15c244b4052bb85547630ceeb6
momijidaaa <m89184412@gmail.com>
2026-04-12 15:39:04 +0900

    crop.js の作成

---
+++ scripts/crop.js
@@ -0,0 +1,29 @@
+import { world, system } from "@minecraft/server";
+
+system.runInterval(() => {
+  const dim = world.getDimension("overworld");
+
+  const players = dim.getPlayers();
+
+  for (const player of players) {
+    const p = player.location;
+
+    const block = dim.getBlock({
+      x: Math.floor(p.x),
+      y: Math.floor(p.y) - 1,
+      z: Math.floor(p.z)
+    });
+
+    if (!block) continue;
+
+    if (block.typeId === "mc:rice_crop") {
+      const age = block.permutation.getState("mc:crop_age");
+
+      if (age < 3 && Math.random() < 0.1) {
+        block.setPermutation(
+          block.permutation.withState("mc:crop_age", age + 1)
+        );
+      }
+    }
+  }
+}, 40);
